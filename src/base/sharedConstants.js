6635bb72104b4344e197fdad11e230c162ea0d79
teramotodaiki <i101323@gmail.com>
2017-05-23 12:42:38 +0900

    initialize commit

---
+++ src/base/sharedConstants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IsProduction = true;
+exports.Build = '1.0';
+exports.ApplicationName = 'Code Connection';
+//# sourceMappingURL=sharedConstants.js.map
